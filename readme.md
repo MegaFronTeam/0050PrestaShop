e66e4d7c6749e0ba840752dbb88b4b9b4693bc38
JulietBrn <shishkina.ja@gmail.com>
2024-10-16 11:51:14 +0200

    feat: add slider, 02 page

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 ### Pages
 1. <https://megafronteam.github.io/0050PrestaShop/>
-2. <https://megafronteam.github.io/0050PrestaShop/00-modal.html>
+2. <https://megafronteam.github.io/0050PrestaShop/02-categories.html>
+
+#### In progress
+1. <https://megafronteam.github.io/0050PrestaShop/03-subcategories.html>
+2. <https://megafronteam.github.io/0050PrestaShop/04-product.html>
+1. <https://megafronteam.github.io/0050PrestaShop/00-modal.html>
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -79,6 +79,112 @@ function eventHandler() {
       }
     }
 	});
+
+  document.querySelectorAll('.sSliderMore').forEach((sliderElement, index) => {
+    const sSliderMore = new Swiper(sliderElement.querySelector(".wrap"), {
+
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: sliderElement.querySelector('.swiper-button-next'),
+        prevEl: sliderElement.querySelector('.swiper-button-prev'),
+      },
+      pagination: {
+        el: sliderElement.querySelector('.swiper-pagination'),
+        type: 'bullets',
+        clickable: true, 
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        },
+        1200: {
+          slidesPerView: 4,
+        }
+      }
+    });
+  })
+
+  function currencyFormat(num) {
+		return num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+	}
+
+  $(".range-wrap--age").each(function () {
+		let _this = $(this);
+
+		var $range = _this.find(".slider-js");
+
+		var $inputFrom = _this.find(".input_from");
+
+		var $inputTo = _this.find(".input_to");
+
+    var $spanFrom = _this.find(".range-from");
+    var $spanTo = _this.find(".range-to");
+
+		var instance,
+			from,
+			to,
+			min = $range.data("min"),
+			max = $range.data("max");
+
+		$range.ionRangeSlider({
+			skin: "round",
+			type: "double",
+			grid: false,
+			grid_snap: false,
+			hide_min_max: true,
+			hide_from_to: false,
+			//here
+			onStart: updateInputs,
+			onChange: updateInputs,
+			onFinish: updateInputs,
+		});
+		instance = $range.data("ionRangeSlider");
+
+		function updateInputs(data) {
+			from = data.from;
+			to = data.to;
+			$inputFrom.prop("value", currencyFormat(from));
+			$inputTo.prop("value", currencyFormat(to));
+
+      $spanFrom.text(from);
+      $spanTo.text(to);
+		}
+
+		$inputFrom.on("change input ", function () {
+			var val = +$(this).prop("value").replace(/\s/g, ""); // validate
+
+			if (val < min) {
+				val = min;
+			} else if (val > to) {
+				val = to;
+			}
+
+			instance.update({
+				from: val,
+			});
+			$(this).prop("value", currencyFormat(val));
+		});
+		$inputTo.on("change input ", function () {
+			var val = +$(this).prop("value").replace(/\s/g, ""); // validate
+
+			if (val < from) {
+				val = from;
+			} else if (val > max) {
+				val = max;
+			}
+
+			instance.update({
+				to: val,
+			});
+			$(this).prop("value", currencyFormat(val));
+		});
+	});
+
 }
 if (document.readyState !== "loading") {
 	eventHandler();
